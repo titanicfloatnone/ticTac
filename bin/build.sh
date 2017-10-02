05af640aa31dbb645db26578fde804c19e7faf4d
Alex Mar <alex16@ru.is>
2017-10-02 13:51:23 +0000

    jar file for junit is here, hierarchy for test is present

---
+++ bin/build.sh
@@ -0,0 +1,4 @@
+# Use this for compiling without jar files.
+javac  -d ../classes ../src/main/java/is/ru/tictactoe/TicTacToe.java
+# Use this for compiling jar files
+#javac -cp ../src/main/resources/joda-time-2.9.9.jar -d ../classes ../src/main/java/is/ru/tictactoe/HelloWorld.java
---
+++ bin/run.sh
@@ -0,0 +1,5 @@
+# Run with jar files:
+# java -cp ../src/main/resources/joda-time-2.9.9.jar:../classes is.ru.tictactoe.TicTacToe
+
+#Run with jar files:
+java ../classes is.ru.tictactoe.TicTacToe
