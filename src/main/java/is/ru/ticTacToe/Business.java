6cb01d315daedff6d45a11bf22a4aae0ae6f4c25
Hafsteinn Stefánsson <hafsteinns16@ru.is>
2017-10-23 18:09:30 +0000

    board and constructer added to Business

--- src/main/java/is/ru/ticTacToe/Business.java
+++ src/main/java/is/ru/ticTacToe/Business.java
@@ -2,6 +2,14 @@ package is.ru.tictactoe;
 
 public class Business {
 
+	private Square[][] board;
+	private boolean xmove;
+
+	public Business(){
+		square = new Square[3][3];
+		xmove = true;
+	}
+
 	public static void main(String[] args) {
 		
 	}
