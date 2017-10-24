f6511d9c8d39cee7843b9dda2193d08edfd97082
SteiniSaem <thorsteinns16@ru.is>
2017-10-24 15:21:42 +0000

    Single X move working

--- src/main/java/is/ru/ticTacToe/Business.java
+++ src/main/java/is/ru/ticTacToe/Business.java
@@ -2,15 +2,29 @@ package is.ru.tictactoe;
 
 public class Business {
 
-	private Square[][] board;
+	private char[] board;
 	private boolean xmove;
 
 	public Business(){
-		board = new Square[3][3];
+		board = new char[9];
 		xmove = true;
+	}
+
+	public char[] getBoard()
+	{
+		return board;
+	}
+
+	public void makeMove(int position)
+	{
+			board[position] = 'X';
+			xmove = false;
 	}
 
 	public static void main(String[] args) {
 		
 	}
 }
+
+
+
--- src/test/java/is/ru/tictactoe/BusinessTest.java
+++ src/test/java/is/ru/tictactoe/BusinessTest.java
@@ -5,4 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 public class BusinessTest {
    
+   @Test
+   public void makeMoveMakeOneXMoveTest()
+   {
+   		Business test = new Business();
+ 		test.makeMove(7);
+ 		assertEquals('X', test.getBoard()[7]);
+   }
+
 }
