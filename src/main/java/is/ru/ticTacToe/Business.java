e6b7234cfee4f83775baf29e224e3f1d8361b6d1
Sindri Ingólfsson <sindriingolfsson@gmail.com>
2017-10-24 14:40:32 +0000

    changed board to single array instead of double

--- src/main/java/is/ru/ticTacToe/Business.java
+++ src/main/java/is/ru/ticTacToe/Business.java
@@ -2,11 +2,11 @@ package is.ru.tictactoe;
 
 public class Business {
 
-	private Square[][] board;
+	private Square[] board;
 	private boolean xmove;
 
 	public Business(){
-		board = new Square[3][3];
+		board = new Square[9];
 		xmove = true;
 	}
 
