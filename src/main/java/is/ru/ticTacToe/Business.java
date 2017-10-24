e66adbeaf8ef41972aa4794485b2f6a6960ba488
Sindri Ingólfsson <sindriingolfsson@gmail.com>
2017-10-24 14:57:43 +0000

    winner for unused board done

--- src/main/java/is/ru/ticTacToe/Business.java
+++ src/main/java/is/ru/ticTacToe/Business.java
@@ -13,4 +13,8 @@ public class Business {
 	public static void main(String[] args) {
 		
 	}
+
+	public static boolean hasWinner() {
+		return false;
+	}
 }
--- src/test/java/is/ru/tictactoe/BusinessTest.java
+++ src/test/java/is/ru/tictactoe/BusinessTest.java
@@ -4,5 +4,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BusinessTest {
-   
+
+   @Test
+   public void hasWinnerEmptyBoardTest() {
+   		Business test = new Business();
+   		assertEquals(false, test.hasWinner());
+   }
 }
