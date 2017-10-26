90e91f081a20fc775877e6fd2e182bb4e26d3e67
Sindri Ingólfsson <sindriingolfsson@gmail.com>
2017-10-26 15:02:37 +0000

    added heroku Port func

--- src/main/java/is/ru/tictactoe/FrontEnd.java
+++ src/main/java/is/ru/tictactoe/FrontEnd.java
@@ -4,13 +4,21 @@ import static spark.Spark.*;
 
 public class FrontEnd {
   public static void main(String[] args) {
-
+  	PORT(getHerokuPort());
     staticFiles.location("/public");
 
     get("/", (req, res) -> greet());
   }
 
   public static String greet() {
-      return "Hello world";
+    return "Hello world";
   }
+
+  static int getHerokuPort() {
+    ProcessBuilder psb = new ProcessBuilder();
+	if (psb.environment().get("PORT") != null) {
+	    return Integer.parseInt(psb.environment().get("PORT"));
+    }
+	return 4567;
+    }
 }
