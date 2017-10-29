08c28a4c897128016aa693b424ad5ef78f9e8b23
Gudberg Sumarlidason <gudberg_sumarlidason@Gudberg.local>
2017-10-29 12:31:15 +0000

    changing the text when there is a win/tie

--- src/main/resources/public/js/ajax.js
+++ src/main/resources/public/js/ajax.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                       if(Data == "true")
                       {
                         if(!hasWinner) {
-                          $("#winner-is").html(winner + object[id].symbol);
+                          $("#winner-is").html(winner + object[id].symbol + "!");
                         }
                         hasWinner = true;
                       }
@@ -53,8 +53,6 @@ $(document).ready(function(){
                   }
                   $("div#game-grid").html(html);
                 }
-
-
 
                 $.ajax({
                   url: '/isTie',
